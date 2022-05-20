822490771fbb9ac3af42bb2e995f82766c5e46a0
Anabil Sarker <sarkeranabil@gmail.com>
2022-05-20 17:15:54 +0600

    Add project files.

---
+++ PB test app/QueryBonds.cs
@@ -0,0 +1,44 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+
+namespace PB_test_app
+{
+    public class QueryBonds
+    {
+        private static readonly QueryBonds _instance = new QueryBonds();
+        public static QueryBonds Instance { get { return _instance; } }
+
+        public ObservableCollection<QueryBonds> QueryValue = new ObservableCollection<QueryBonds>();
+
+        public string ResponseValue { get; set; }
+
+        public async void PostRequest(string value)
+        {
+            var values = new Dictionary<string, string>
+            {
+                { "gsearch", "value" }
+            };
+            var content = new FormUrlEncodedContent(values);
+
+            using (HttpClient client = new HttpClient())
+            {
+                using (var response = await client.PostAsync("https://www.bb.org.bd/en/index.php/investfacility/prizebond", content))
+                {
+                    var responseString = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseString);
+                    QueryValue.Add(new QueryBonds { ResponseValue = responseString });
+                }
+            }
+        }
+    }
+}
---
+++ Prize Bond Checker/MainActivity.cs
@@ -0,0 +1,52 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using Google.Android.Material.BottomNavigation;
+
+namespace Prize_Bond_Checker
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, BottomNavigationView.IOnNavigationItemSelectedListener
+    {
+        RelativeLayout relativeLayout;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            relativeLayout = FindViewById<RelativeLayout>(Resource.Id.parent_container);
+            relativeLayout.FindViewById(Resource.Id.child_container_home).Visibility = ViewStates.Visible;
+            BottomNavigationView navigation = FindViewById<BottomNavigationView>(Resource.Id.navigation);
+            navigation.SetOnNavigationItemSelectedListener(this);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        public bool OnNavigationItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.navigation_home:
+                    relativeLayout.FindViewById(Resource.Id.child_container_prizebonds).Visibility = ViewStates.Gone;
+                    relativeLayout.FindViewById(Resource.Id.child_container_home).Visibility = ViewStates.Visible;
+                    return true;
+                case Resource.Id.navigation_prizebond:
+                    relativeLayout.FindViewById(Resource.Id.child_container_home).Visibility = ViewStates.Gone;
+                    relativeLayout.FindViewById(Resource.Id.child_container_prizebonds).Visibility = ViewStates.Visible;
+                    return true;
+                case Resource.Id.navigation_notifications:
+                    return true;
+            }
+            return false;
+        }
+    }
+}
+
